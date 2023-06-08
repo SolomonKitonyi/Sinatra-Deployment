301a18dbde691f4b364f8ed11ac43b24961f5d47
SolomonKitonyi <kitonyisolomon@gmail.com>
2023-06-08 13:04:41 +0300

    add changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Room.create(name: "Room1",room_no:101)
+Room.create(name: "Room2",room_no:103)
+Room.create(name: "Room1",room_no:104)
 
 puts "✅ Done seeding!"
